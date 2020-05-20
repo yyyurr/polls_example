355f6b83cb0a5a064aa44ed9c5e8b895b96113ad
TechNote <yurr22@naver.com>
2020-05-20 15:44:27 +0900

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\wonyong\pycharmprojects\polls_example\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
